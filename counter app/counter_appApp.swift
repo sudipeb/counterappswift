5f56a61d34d4a7f32028ed953523f3221c00aa45
sudipeb <sudip.koirala@ebpearls.com>
2026-02-06 14:29:25 +0545

    Initial Commit

---
+++ counter app/counter_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  counter_appApp.swift
+//  counter app
+//
+//  Created by ebpearls on 06/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct counter_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

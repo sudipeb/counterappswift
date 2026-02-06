d5a7667fb86380cb435cef386325a10b4fa906e2
sudipeb <sudip.koirala@ebpearls.com>
2026-02-06 14:38:33 +0545

    created counter mechanism

--- counter app/ContentView.swift
+++ counter app/ContentView.swift
@@ -4,16 +4,36 @@
 //
 //  Created by ebpearls on 06/02/2026.
 //
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var count = 0
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        VStack(spacing: 20) {
+            Image(systemName: "number.circle")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+            Text("Count: \(count)")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+
+            HStack(spacing: 20) {
+                Button {
+                    count -= 1
+                } label: {
+                    Image(systemName: "minus.circle.fill")
+                        .font(.largeTitle)
+                }
+
+                Button {
+                    count += 1
+                } label: {
+                    Image(systemName: "plus.circle.fill")
+                        .font(.largeTitle)
+                }
+            }
         }
         .padding()
     }
